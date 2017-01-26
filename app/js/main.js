e775d6b5810883b747ac45ee1a7b7e7fb8643d2c
Mathew Morris <morrismat45@gmail.com>
2017-01-26 01:57:25 -0600

    added background to nav when scroll

--- app/js/main.js
+++ app/js/main.js
@@ -36,9 +36,28 @@ $(document).ready(function() {
     // initializeClock('countdown', deadline);
     $("#mobile-menu-button").click(function() {
         $(this).toggleClass('open');
-        $('.menu').slideToggle();
+        $('.menu').slideToggle(600);
     })
 
 
+
+
+
+
+
+})
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 50) {
+        $(".navbar").animate({
+            backgroundColor: 'rgba(0,0,0,.8)',
+            paddingBottom: '.5em'
+        }, 200)
+    } else {
+        $(".navbar").animate({
+            backgroundColor: 'rgba(0,0,0,0)',
+            paddingBottom: '0'
+        }, 200)
+    }
 
 })
