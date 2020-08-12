aabb860363e688a84c3154ec071bc0f775c017d7
Mathew Morris <hello@mathewmorris.com>
2020-08-12 00:25:19 -0500

    Initial content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,50 @@
 import React from 'react';
 
 export default function Home() {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <div>
+        <div>Logo</div>
+        <nav>
+          <ul>
+            <li>Blog</li>
+            <li>Biking</li>
+            <li>Electronics</li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <h1>Mathew Morris</h1>
+        <p>
+          I&apos;m a mountain biking fool here in Oklahoma City, OK &mdash; Yes,
+          it&apos;s flat, but I love to go to Bentonville, AR for some sweet
+          dirt hills.
+        </p>
+        <p>
+          For the past (number of years) years, I&apos;ve worked in front end
+          development across a variety of industries. I am currently an Engineer
+          at Ittybam, building successful enterprise applications.
+        </p>
+        <p>
+          I occasionally take on freelance and contract projects. If you&apos;re
+          looking for that sort of thing, feel free to reach out.
+        </p>
+      </div>
+      <div>
+        <div>
+          <p>Copyright $mdash; Mathew Morris</p>
+          <p>Quote</p>
+        </div>
+        <div>
+          <ul>
+            <li>RSS</li>
+            <li>Instagram</li>
+            <li>Twitter</li>
+            <li>Github</li>
+            <li>Email</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
